1c5931e3d7a0fbd9a04b03996e18f69d7b786bc5
Dmitriy Minaev <minaevdup@gmail.com>
2020-01-13 19:54:32 +0300

    Proxy api requests to the backend server

--- src/server.js
+++ src/server.js
@@ -2,16 +2,26 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import proxy from 'http-proxy-middleware';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const app = polka();
+
+app.use(compression({ threshold: 0 }), sirv('static', { dev }));
+
+app.use(
+	'/api',
+	proxy({
+		target: process.env.URL_API,
+		pathRewrite: {
+			'^/api/': ''
+		},
+		changeOrigin: true
+	})
+);
+
+app.listen(PORT, err => {
+	if (err) console.log('error', err);
+});

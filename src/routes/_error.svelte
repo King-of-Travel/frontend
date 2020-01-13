7cd914f7a7bbb7f0247600401abb2f81ab46d885
Dmitriy Minaev <minaevdup@gmail.com>
2020-01-13 16:42:11 +0300

    Install eslint, husky

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -2,11 +2,12 @@
 	export let status;
 	export let error;
 
-	const dev = process.env.NODE_ENV === 'development';
+	const dev = 'process.env.NODE_ENV' === 'development';
 </script>
 
 <style>
-	h1, p {
+	h1,
+	p {
 		margin: 0 auto;
 	}
 
@@ -36,5 +37,5 @@
 <p>{error.message}</p>
 
 {#if dev && error.stack}
-	<pre>{error.stack}</pre>
+<pre>{error.stack}</pre>
 {/if}

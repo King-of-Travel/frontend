b4d59db5b70b1887f17238bba0d35821c48ca2f2
Dmitriy Minaev <minaevdup@gmail.com>
2020-01-14 21:31:19 +0300

    feat: positioning the main layout, global styles for the form

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,14 @@
-<slot></slot>
+<main>
+  <slot></slot>
+</main>
 
 <style global>
-	@import '../global.css';
+  @import '../global.css';
+
+  main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 </style>

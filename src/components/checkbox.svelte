06a790be77442a37683bee3b6acc14d29f546f2f
Dmitriy Minaev <minaevdup@gmail.com>
2020-01-20 19:21:56 +0300

    Add checkbox and icon

---
+++ src/components/checkbox.svelte
@@ -0,0 +1,83 @@
+<span>
+  <input bind:checked {name} {id} type="checkbox" tabindex="-1" />
+  <label for="{id}" tabindex="0" use:enter="{keystroke}">
+    <i>
+      {#if checked}
+        <Icon size="16">
+          <CheckIcon />
+        </Icon>
+      {/if}
+    </i>
+    {label}
+  </label>
+</span>
+
+<script>
+  import CheckIcon from './icons/check.svelte';
+  import Icon from './icon.svelte';
+
+  export let checked, id, name, label;
+
+  function keystroke() {
+    checked = !checked;
+  }
+
+  function enter(node, callback) {
+    function onkeydown(event) {
+      if (event.which === 13) callback(node);
+    }
+
+    node.addEventListener('keydown', onkeydown);
+
+    return {
+      destroy() {
+        node.removeEventListener('keydown', onkeydown);
+      }
+    };
+  }
+</script>
+
+<style>
+  span {
+    position: relative;
+    vertical-align: baseline;
+    /* height icon + border */
+    height: 40px;
+    line-height: 40px;
+  }
+
+  input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    z-index: 1;
+  }
+
+  label {
+    position: relative;
+    display: block;
+    padding-left: calc(20px + 5px);
+    cursor: pointer;
+    user-select: none;
+  }
+
+  label:active > i {
+    transform: scale(0.9);
+    transition: transform 0.1s;
+  }
+
+  i {
+    position: absolute;
+    top: 10px;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 20px;
+    height: 20px;
+    border: var(--base-border-w) solid var(--base-accent);
+    border-radius: var(--base-border-r);
+    color: var(--base-accent);
+  }
+</style>
---
+++ src/components/icon.svelte
@@ -0,0 +1,18 @@
+<svg
+  {viewBox}
+  width="{size}"
+  height="{size}"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  stroke="{color}"
+  stroke-width="2"
+  stroke-linecap="round"
+>
+  <slot></slot>
+</svg>
+
+<script>
+  export let size = 24,
+    color = 'currentColor',
+    viewBox = '0 0 24 24';
+</script>

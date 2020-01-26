87d54c541b675a8f4b936e51837f6718dfa28605
Dmitriy Minaev <minaevdup@gmail.com>
2020-01-26 16:16:21 +0300

    Meta description, logo

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,13 +1,21 @@
+<svelte:head>
+  <meta name="theme-color" content="#12aef7" />
+</svelte:head>
+
+<Header />
 <main>
   <slot />
 </main>
+
+<script>
+  import Header from 'components/header/index.svelte';
+</script>
 
 <style global>
   @import '../global.css';
 
   main {
     width: 100%;
-    height: 100%;
     display: flex;
     flex-direction: column;
   }

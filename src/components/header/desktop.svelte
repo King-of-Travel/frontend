da56ee97b3859d52259ad75710677bdd8b9ed9cf
Dmitriy Minaev <minaevdup@gmail.com>
2020-01-30 17:37:03 +0300

    Add padding for header

--- src/components/header/desktop.svelte
+++ src/components/header/desktop.svelte
@@ -1,37 +1,39 @@
 <header>
-  <a href="/" rel="prefetch" class="logo">
-    <img
-      src="images/logo-64.png"
-      alt="King of Travel Logotype"
-      height="50"
-      width="50"
-    />
-  </a>
-  <nav>
-    {#if user}
-      <Dropdown width="150px" ariaLabel="View profile and more">
-        <div slot="summary" class="profile">
-          <img
-            src="/images/no-avatar.png"
-            alt="Our avatar"
-            height="20"
-            width="20"
-          />
-          <span class="profile__triangle"></span>
-        </div>
+  <div>
+    <a href="/" rel="prefetch" class="logo">
+      <img
+        src="images/logo-64.png"
+        alt="King of Travel Logotype"
+        height="50"
+        width="50"
+      />
+    </a>
+    <nav>
+      {#if user}
+        <Dropdown width="150px" ariaLabel="View profile and more">
+          <div slot="summary" class="profile">
+            <img
+              src="/images/no-avatar.png"
+              alt="Our avatar"
+              height="20"
+              width="20"
+            />
+            <span class="profile__triangle"></span>
+          </div>
 
-        <DropdownItem href="/users/{user.username}" rel="prefetch">
-          Your profile
-        </DropdownItem>
-        <DropdownItem href="/editor" rel="prefetch">Create post</DropdownItem>
-        <DropdownItem href="/trip/add" rel="prefetch">Add trip</DropdownItem>
-        <DropdownItem href="/settings" rel="prefetch">Settings</DropdownItem>
-      </Dropdown>
-    {:else}
-      <a href="/join" rel="prefetch" class="button">Sign up</a>
-      <a href="/login" rel="prefetch" class="button">Login</a>
-    {/if}
-  </nav>
+          <DropdownItem href="/users/{user.username}" rel="prefetch">
+            Your profile
+          </DropdownItem>
+          <DropdownItem href="/editor" rel="prefetch">Create post</DropdownItem>
+          <DropdownItem href="/trip/add" rel="prefetch">Add trip</DropdownItem>
+          <DropdownItem href="/settings" rel="prefetch">Settings</DropdownItem>
+        </Dropdown>
+      {:else}
+        <a href="/join" rel="prefetch" class="button">Sign up</a>
+        <a href="/login" rel="prefetch" class="button">Login</a>
+      {/if}
+    </nav>
+  </div>
 </header>
 
 <script>
@@ -43,16 +45,20 @@
 
 <style type="postcss">
   header {
-    display: grid;
-    height: 65px;
-    width: 100%;
-    grid-template-columns: 50px minmax(auto, 850px);
-    justify-content: center;
-    align-items: center;
-    padding: 0 15px;
     z-index: 100;
     background-color: #b7e7fd;
     box-shadow: 0 0 15px 0px #b7e7fd;
+  }
+
+  header > div {
+    display: grid;
+    grid-template-columns: 50px 1fr;
+    justify-content: center;
+    align-items: center;
+    height: 65px;
+    max-width: 900px;
+    margin: 0 auto;
+    padding: 0 15px;
   }
 
   @media (--mobile) {

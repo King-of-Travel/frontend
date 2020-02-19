74179145f727e9235a0726f16e64b94e36560263
Dmitriy Minaev <minaevdup@gmail.com>
2020-02-19 14:53:40 +0300

    Button to go to edit the article, display the country and city

--- src/routes/a/[articleId].svelte
+++ src/routes/a/[articleId].svelte
@@ -5,25 +5,56 @@
 <main class="center-content">
   <section>
     <article>
-      <header class="block">
-        <div class="meta">
-          <a
-            href="/users/{article.user.username}"
-            rel="prefetch"
-            title="Posted by"
-            class="link"
-          >
-            {isCurrentUserAuthor ? 'You' : article.user.username}
-          </a>
-          <span title="Publication date">
-            {@html articleCreatedAt(article.createdAt)}
-          </span>
+      <header>
+        <div class="left-block">
+          <div class="article-author">
+            <a
+              href="/users/{article.user.username}"
+              rel="prefetch"
+              title="Posted by"
+              class="link"
+            >
+              {isCurrentUserAuthor ? 'You' : article.user.username}
+            </a>
+
+            <span title="Publication date" class="created-date">
+              {@html articleCreatedAt(article.createdAt)}
+            </span>
+          </div>
+
+          {#if article.countryCode}
+            <div class="article-place">
+              <span class="country">
+                Country: {Countries.find(country => country.value === article.countryCode).label}
+              </span>
+
+              {#if article.city}
+                <span class="city">, City: {article.city}</span>
+              {/if}
+            </div>
+          {/if}
         </div>
 
-        <h1>{article.title}</h1>
+        <div class="right-block">
+          {#if isCurrentUserAuthor}
+            <a
+              href="/editor/{article.id}"
+              rel="prefetch"
+              class="button edit"
+              title="Edit article"
+            >
+              Edit
+            </a>
+          {/if}
+        </div>
       </header>
+
+      <h1>{article.title}</h1>
+
       {@html article.body}
+
     </article>
+
     <footer>
       <div class="left-block">
         <Share />
@@ -62,6 +93,7 @@
   import { setContext } from 'svelte';
   import { articleCreatedAt } from 'date-formatting';
 
+  import Countries from 'components/locales/countries/en.json';
   import Likes from './_likes.svelte';
   import Share from './_share.svelte';
 
@@ -79,24 +111,42 @@
     margin: 0 auto;
   }
 
-  .meta {
+  header {
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    grid-gap: 15px;
     font-size: var(--base-h6);
-    margin-bottom: 5px;
+    color: var(--base-gray);
   }
 
-  .meta span {
+  .article-author .created-date {
     margin-left: 5px;
-    color: var(--base-gray);
     text-transform: lowercase;
+  }
+
+  .article-place {
+    margin-top: 5px;
+  }
+
+  .right-block {
+    text-align: end;
+  }
+
+  .right-block .edit {
+    padding: 5px 10px;
+    border: var(--base-border-w) solid var(--base-border);
+    background: transparent;
+  }
+
+  .right-block .edit:focus,
+  .edit:hover {
+    background-color: var(--base-hover);
+    border-color: var(--base-hover);
   }
 
   footer {
     display: grid;
     grid-template-columns: 1fr 1fr;
     margin-top: 15px;
-  }
-
-  .right-block {
-    text-align: end;
   }
 </style>

6cb48e1644928f7f5d902ecdc019e47aa9c00f1f
Dmitry Minaev <mnvdmitry@gmail.com>
2020-06-13 21:13:07 +0300

    Change the positioning of filters
    On the mobile version, article filters do not fit into the container and go beyond it

--- src/components/filter/item/button.svelte
+++ src/components/filter/item/button.svelte
@@ -3,6 +3,7 @@
     on:click
     disabled="{isActive}"
     aria-current="{isActive ? ariaCurrent : ''}"
+    class="button"
   >
     <slot />
   </button>
@@ -12,3 +13,22 @@
   export let isActive = false,
     ariaCurrent = 'page';
 </script>
+
+<style>
+  button {
+    --background: none;
+
+    padding: 9px;
+    font-weight: 600;
+    border-radius: 0;
+  }
+
+  button:disabled {
+    --background: var(--base-accent);
+  }
+
+  button:not(:disabled):hover,
+  button:not(:disabled):focus {
+    --background: var(--base-hover) !important;
+  }
+</style>
--- src/components/filter/main-layout.svelte
+++ src/components/filter/main-layout.svelte
@@ -4,11 +4,19 @@
 
 <style>
   nav {
+    --gap: var(--text-side);
+
     display: flex;
+    flex-wrap: wrap;
+    width: calc(100% + var(--gap));
+    margin: calc(-1 * var(--gap)) 0 0 calc(-1 * var(--gap));
   }
 
   nav :global(ul) {
     display: inline-flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: var(--gap) 0 0 var(--gap);
     list-style: none;
     border: var(--base-border-w) solid var(--base-accent);
     border-radius: var(--base-border-r);
@@ -16,44 +24,5 @@
 
   nav :global(li) {
     font-size: 0.8rem;
-  }
-
-  nav :global(a) {
-    display: block;
-    padding: 9px;
-    font-weight: 600;
-    color: var(--base-text);
-    text-shadow: none;
-    background: none;
-  }
-
-  nav :global(button) {
-    --background: none;
-
-    padding: 9px;
-    font-weight: 600;
-    border-radius: 0;
-  }
-
-  nav :global(ul:not(:last-child)) {
-    margin-right: var(--text-side);
-  }
-
-  nav :global(a:hover, a:focus) {
-    box-shadow: none;
-  }
-
-  nav :global(a.active) {
-    pointer-events: none;
-    cursor: default;
-    background-color: var(--base-accent);
-  }
-
-  nav :global(button:disabled) {
-    --background: var(--base-accent);
-  }
-
-  nav :global(button:not(:disabled):hover, button:not(:disabled):focus) {
-    --background: var(--base-hover) !important;
   }
 </style>
